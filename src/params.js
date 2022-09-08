7cd399488c34292c585bb4e35870db7c1b1661a3
Lívia Cardoso <livia.cardoso@superior-ind.com>
2022-09-08 15:24:54 -0300

    first commit - mines

---
+++ src/params.js
@@ -0,0 +1,9 @@
+import {Dimensions} from 'react-native';
+
+const params = {
+  blockSize: 30,
+  borderSize: 5,
+  fontSize: 15,
+  headerRatio: 0.15, //15% da  tela vai representar o cabeçalho - Proporção do painel superior da tela
+  difficultLevel: 0.1, //O nível de dificuldade será 1% em cima da quantidade de campos que estão na tela
+};
